7d1ad0410b7d5fd3664399d2af8a583019b908a3
jellybeansenjoyer <therealraghavkashyap26@gmail.com>
2023-07-03 01:52:13 +0530

    splash screen

--- app/src/main/java/com/example/ubercloneapp/SplitScreenActivity.kt
+++ app/src/main/java/com/example/ubercloneapp/SplitScreenActivity.kt
@@ -2,10 +2,20 @@ package com.example.ubercloneapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        delaySplashScreen()
+    }
+    fun delaySplashScreen(){
+        lifecycleScope.launch {
+            delay(5000)
+            Toast.makeText(this@SplashScreenActivity, "Splash Screen Done!!", Toast.LENGTH_SHORT).show()
+        }
     }
 }
